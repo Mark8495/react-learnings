d123bc9e68ada6156c682516eb1f23aa902ca01d
Mark Byrne <markbyrne8495@gmail.com>
2024-03-26 11:32:52 +0000

    react learning

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navbar(){
+    return (
+        <main>
+            <h1>Fun Facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originaly created by Jordan Walke</li>
+                <li>Has well over 100k stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+            </ul>
+        </main>
+    )
+}
