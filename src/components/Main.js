1c6d522c36033228b4c2f22d7c90ba54fe81b07b
Mark Byrne <markbyrne8495@gmail.com>
2024-03-27 10:31:45 +0000

    react facts

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,17 @@
 import React from "react";
+import reactlogo from "../images/logo512.png"
 
 export default function Navbar(){
     return (
         <main>
-            <h1>Fun Facts about React</h1>
-            <ul>
+            <h1 className="main--title">Fun Facts about React</h1>
+            <ul className="main-facts">
                 <li>Was first released in 2013</li>
                 <li>Was originaly created by Jordan Walke</li>
                 <li>Has well over 100k stars on GitHub</li>
                 <li>Is maintained by Facebook</li>
             </ul>
+            {/* <img src={reactlogo} className="main--icon"/> */}
         </main>
     )
 }
